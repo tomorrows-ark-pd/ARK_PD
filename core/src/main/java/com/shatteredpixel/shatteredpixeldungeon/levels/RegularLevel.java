b3c98e1d04efb17f9acc8cf6274653550d81415f
Jun Lee <dan.junwoo.lee@gmail.com>
2026-07-27 22:28:49 -0400

    0.5.3: minishop guarantee

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/RegularLevel.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/RegularLevel.java
@@ -142,7 +142,8 @@ public abstract class RegularLevel extends Level {
 
         if (Dungeon.shopOnLevel())
             initRooms.add(new ShopRoom());
-        else if (Random.Int(80) == 0) {
+        //else if (Random.Int(80) == 0) {
+        else if() {
             initRooms.add(new MiniShopRoom());
         }
 
