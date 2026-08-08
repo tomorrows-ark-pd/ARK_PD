5ef268306a07ea09a94b526bc5517d1b0e263990
PATRIOT-BALL <gkdlsptmxm@naver.com>
2026-08-08 15:16:03 +0900

    Update RegularLevel.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/RegularLevel.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/RegularLevel.java
@@ -142,7 +142,7 @@ public abstract class RegularLevel extends Level {
 
         if (Dungeon.shopOnLevel())
             initRooms.add(new ShopRoom());
-        else {
+        else if (Random.Int(80) == 0) {
             initRooms.add(new MiniShopRoom());
         }
 
